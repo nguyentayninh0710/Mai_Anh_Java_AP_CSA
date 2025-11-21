c523604e204c9c2213655da09fa826e294bae3c6
Huynh Ky Khoi Nguyen <huynhkykhoinguyen0710@gmail.com>
2025-11-21 14:50:52 +0700

    add question

--- src/Day02_Exercise_2/RectangleTester.java
+++ src/Day02_Exercise_2/RectangleTester.java
@@ -1,5 +1,41 @@
 package Day02_Exercise_2;
+/*
+ Exercise 2 – Rectangle Attributes and Multiple Objects
 
+A drawing program stores simple rectangles.
+You will model rectangles and compare their areas.
+
+Specifications
+
+Write a class called Rectangle with these attributes:
+
+double widthInCm – width in centimeters
+
+double heightInCm – height in centimeters
+
+final double CM_PER_INCH = 2.54 – constant for converting inches to centimeters
+
+In a separate RectangleTester class with a main method:
+
+Create one Rectangle object called screen that represents a small screen:
+
+width = 8.0 inches
+
+height = 5.0 inches
+
+Store the values in centimeters using CM_PER_INCH.
+
+Create a second Rectangle object called paper:
+
+width = 21.0 centimeters
+
+height = 29.7 centimeters
+
+Compute the area of each rectangle (in square centimeters) using the attributes:
+area = widthInCm * heightInCm
+
+Print both areas and then print which object has the larger area: "screen" or "paper". 
+ */
 public class RectangleTester {
 
 }
